004fd0e6f5757cc3fe228cee445bf4a38b54d0a5
Paras Nagpal <paras.nagpal743@gmail.com>
2022-12-09 02:08:42 +0530

    first commit

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Dashboard,Error,Landing,Register } from "./pages";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/landing" element={<Landing/>} />
+      <Route path="*" element={<Error/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+export default App
---
+++ FrontEnd/src/components/Logo.js
@@ -0,0 +1,7 @@
+import logo from '../assets/images/JobaMon.png'
+
+const Logo = () =>{
+   //return <img src={logo} alt='jobamon' className='logo'/>
+}
+
+export default Logo
