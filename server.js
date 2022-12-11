5a606a588e36596a5b64d3803a51114f42502ef8
Paras Nagpal <parasnagpal0707@192.168.1.7>
2022-12-11 20:30:51 +0530

    added middleware

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import notFountMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
+import connectDB from './db/connect.js'
 const app = express() 
+dotenv.config()
+
+// middlewares
+notFountMiddleware , errorHandlerMiddleware
 
 app.get('/',(req,res)=>{
-    res.send('Hello world')
+    res.send('hello world!')
 })
 
+// Using Middlewares 
+app.use(notFountMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 5000 ; 
-app.listen(port,()=>{
-    console.log(`server is sucessfully running on port ${port}`)
-})
+const port = process.env.PORT || 8000 
+
+const start = async() =>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port,()=>{
+            console.log(`server is sucessfully running on port ${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
